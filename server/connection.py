af1ffe694325e4c8e9e21038aab52db91c41e34f
Ben O'Bryan <obryanbw@gmail.com>
2022-03-26 22:19:41 -0500

    Clients can connect to server

---
+++ server/connection.py
@@ -0,0 +1,36 @@
+import threading
+
+
+class Connection(threading.Thread):
+    uuids = []
+
+    def __init__(self, client):
+        super().__init__()
+        self.client = client
+        self.name = ""
+        self.uuid = ""
+
+    def setName(self, name):
+        self.name = name
+        uuid = name
+        if name in Connection.uuids:
+            new_name = name + "1"
+            self.setName(name, new_name)
+            pass
+        else:
+            self.uuid = uuid
+            Connection.uuids.append(self.uuid)
+
+
+    def getNames(self):
+        return self.name, self.uuid
+
+    def getName(self):
+        data: str = self.client.recv(1024).decode()
+        self.setName(data.strip())
+        return self.name
+
+
+    def send(self, message):
+        self.client.send(message.encode())
+
